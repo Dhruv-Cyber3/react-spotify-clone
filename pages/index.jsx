246dfaf11f749da4b3e95676c7889aca3b62d6f3
Dhruv-Cyber3 <totallynoobcoder@gmail.com>
2022-02-06 15:20:07 +0530

    added nextjs middleware, added spotify authentication.

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="h-screen overflow-hidden bg-black">
+      <main className="">
+        <Sidebar />
+        {/* center */}
+      </main>
+      <div>{/* player */}</div>
+    </div>
+  )
+}
